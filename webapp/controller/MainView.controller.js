0318dfaced2f366590a3fe0c66f8e13184620a18
Audrey Shen Laurence Bacar <audrey.shen.l.dauzon@accenture.com>
2026-05-16 19:04:54 +0000

    for exer6_fragment

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -12,10 +12,30 @@ sap.ui.define([
         onInit() {
         },
             onAddItem: function (){
-                var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
-                var sMsg = oTextBundle.getText("addButtonMsg");
-                this.fnDisplayMsg(sMsg);
+                // Comment this code for now
+                // var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+                // var sMsg = oTextBundle.getText("addButtonMsg");
+                // this.fnDisplayMsg(sMsg);
+
+                // Instantiate the fragment
+
+                // create dialog lazily
+                if (!this.oDialog) {
+                    // By using loadFragment, we are adding the fragment as a dependent to the View
+                    // By doing so, we can use the functions inside the view's controller
+                    this.oDialog = this.loadFragment({
+                        name: "com.training.exer5bacar.fragment.ProductDialog"
+                    });
+                } 
+                this.oDialog.then(function(oDialog) {
+                    oDialog.open();
+                });
             },
+            onCloseDialog: function (){
+                this.getView().byId("idProductDialog").close();
+            },
+
+
 
             fnDisplayMsg: function (sMsg){
                 MessageToast.show(sMsg);
@@ -25,7 +45,7 @@ onChangeMOP: function (oEvent) {
 
     var sSelectedKey = oEvent.getParameter("selectedItem").getProperty("key");
     var sSelectedtext = oEvent.getParameter("selectedItem").getProperty("text");
-var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle()
+    var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle()
     var oMobileLabel = this.getView().byId("idLblPhone");
     var oMobileInput = this.getView().byId("idInputPhone");
 
@@ -78,20 +98,31 @@ var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle()
 },
 
 
-onPressCheckout: function (){
-                var oInputFNameValue = this.getView().byId("idInptFName").getValue();
-var oInputLnameValue = this.getView().byId("idInptLName").getValue();
-                // Check if first name is blank
-                var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
-                    var sMsreq = oTextBundle.getText("requiredMsg");
-    var sMsgcheckout = oTextBundle.getText("checkoutMsg");
-                if (oInputFNameValue === "" || oInputLnameValue === "") {
-                    sap.m.MessageToast.show(sMsreq); 
-                }
-                else {
-                    sap.m.MessageToast.show(sMsgcheckout); 
+     onPressCheckout: function (){
+                var oInputFName = this.getView().byId("idInptFName");
+                var oInputLName = this.getView().byId("idInptLName");
+                var oInputFNameValue = oInputFName.getValue();
+                var oInputLNameValue = oInputLName.getValue();
+                var oRouter = this.getOwnerComponent().getRouter();
+
+                // Check if first name and last name is blank
+                if (oInputFNameValue === "" || oInputLNameValue === ""){
+                   
+// set value state to Error
+                    oInputFName.setValueState("Error");
+                    oInputLName.setValueState("Error");
+                } else {
+                    oInputFName.setValueState("None");
+                    oInputLName.setValueState("None");
+
+                    //Navigate to review page passing first
+                    oRouter.navTo("RouteReviewPage", {
+                        firstName: oInputFNameValue
+                    });
+
                 }
             },
+
 
     });
 });
